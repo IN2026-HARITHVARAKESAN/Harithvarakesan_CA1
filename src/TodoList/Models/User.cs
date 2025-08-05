3b044aeb40992095c7b3aac45497fbe4d1988271
Harithvarakesan Sakthivel <Harithvarakesan.bs@solitontech.com>
2025-08-05 15:12:37 +0530

    feat: Implements model structure

---
+++ src/TodoList/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoList.Models
+{
+    /// <summary>
+    /// Blueprint of user data
+    /// </summary>
+    internal class User
+    {
+        /// <summary>
+        /// Gets or sets Id of the user
+        /// </summary>
+        /// <value>User Id</value>
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of the user
+        /// </summary>
+        /// <value>Name of the user</value>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets password of the user
+        /// </summary>
+        /// <value>User Account Password</value>
+        public string? Password { get; set; }
+    }
+}
